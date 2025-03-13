1f8b536f5df96a42ebecb56809c03d0a3797cf3e
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2025-03-13 12:19:31 +0700

    Complete Projects Section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Navbar } from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
 import { Home } from './components/sections/Home';
 import { About } from './components/sections/About';
+import { Projects } from './components/sections/Projects';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -13,13 +14,14 @@ function App() {
 
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />} {" "}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
       {/* Use `` to inject js */}
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <Home />
-        <About />      
+        <About />
+        <Projects />
       </div>
     </>
   );
--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,5 +1,5 @@
-export const About =()=>{
-    const frontendSkills = ["React", "TypeScript", "TailwindCSS", "Bootstrap", "JavaScript" ];
+export const About = () => {
+    const frontendSkills = ["React", "TypeScript", "TailwindCSS", "Bootstrap", "JavaScript"];
 
     const backendSkills = [".NET Core API", "MVC", "Node.js", "MongoDB", "MSSQL"];
 
@@ -20,8 +20,8 @@ export const About =()=>{
                         <div className="rounded-xl p-6 hover:translate-y-1 transition-all">
                             <h3 className="text-xl font-bold mb-4">Frontend</h3>
                             <div className="flex flex-wrap gap-2">
-                                {frontendSkills.map((tech, key)=>(
-                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                                {frontendSkills.map((tech, key) => (
+                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
                                         {tech}
                                     </span>
                                 ))}
@@ -31,8 +31,8 @@ export const About =()=>{
                         <div className="rounded-xl p-6 hover:translate-y-1 transition-all">
                             <h3 className="text-xl font-bold mb-4">Backend</h3>
                             <div className="flex flex-wrap gap-2">
-                                {backendSkills.map((tech, key)=>(
-                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                                {backendSkills.map((tech, key) => (
+                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
                                         {tech}
                                     </span>
                                 ))}
@@ -40,7 +40,7 @@ export const About =()=>{
                         </div>
                     </div>
                 </div>
-                
+
                 {/* Education and Work Experience */}
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
                     <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
@@ -56,18 +56,17 @@ export const About =()=>{
                     </div>
                     <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
                         <h3 className="text-xl font-bold mb-4"> Work Experience </h3>
-                    </div>
-                    <div className="space-y-4 text-gray-300">
-                        <div>
-                            <h4 className="font-semibold">Software Engineer Intern at Silverpond (2024)</h4>
+                        <div className="space-y-4 text-gray-300">
+                            <div>
+                                <h4 className="font-semibold">Software Engineer Intern at Silverpond (2024)</h4>
+                            </div>
+                            <p>
+                                Something something
+                            </p>
                         </div>
-                        <p>
-                            Something something
-                        </p>
                     </div>
                 </div>
             </div>
-
         </section>
     );
 };
