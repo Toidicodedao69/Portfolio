fff342137fff79b7925f06f82166acde25023858
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2025-03-14 19:30:16 +0700

    Change bg color

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -25,17 +25,16 @@ export const LoadingScreen = ({onComplete}) => {
     }, [onComplete]);
 
     return (
-    <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
+    <div className="fixed inset-0 z-50 bg-slate-800 text-gray-100 flex flex-col items-center justify-center">
         <div className="mb-4 text-4xl font-mono font-bold">
             {text} <span className="animate-blink ml-1"> | </span>
         </div>
 
         {/* Loading bar container */}
-        <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+        <div className="w-[200px] h-[3px] bg-gray-800 rounded relative overflow-hidden">
             {/* Loading animation */}
             <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
         </div>
-
     </div>
     );
 }
