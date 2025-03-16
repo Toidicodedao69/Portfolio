6bc112d2bd213c5f6806846a6bb60a0e7afb6901
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2025-03-16 07:43:39 +0700

    Fix overflow buttons and navbar on mobile
    Fix overflow contact inputs in mobile

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -25,25 +25,27 @@ export const Contact = () => {
     return (
         <section id="contact" className="min-h-screen flex items-center justify-center py-20">
             <RevealOnScroll>
-                <div className="px-4 w-150">
+                <div className="px-4 mx-auto w-xl">
                     <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                         Get In Touch
                     </h2>
                     <form className="space-y-6" onSubmit={HandleSubmit}>
-                        <div className="relative">
+                        <div className="relative mx-25 md:mx-auto">
                             <input type="text" id="subject" name="subject" required value={formData.subject} className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5" placeholder="Subject" onChange={(e) => setFormData({ ...formData, subject: e.target.value })} />
                         </div>
-                        <div className="relative">
+                        <div className="relative mx-25 md:mx-auto">
                             <input type="email" id="email" name="email" required value={formData.email} className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5" placeholder="youremail@gmail.com" onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
                         </div>
-                        <div className="relative">
+                        <div className="relative mx-25 md:mx-auto">
                             <textarea id="message" name="message" required value={formData.message} rows={5} className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5" placeholder="Enter Your Messages" onChange={(e) => setFormData({ ...formData, message: e.target.value })}>
                             </textarea>
                         </div>
-
-                        <button type="submit" className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,140,246,0.5)]">
-                            Send Message
-                        </button>
+                        
+                        <div className="mx-25 md:mx-auto">
+                            <button type="submit" className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,140,246,0.5)]">
+                                Send Message
+                            </button>
+                        </div>
                     </form>
                 </div>
 
