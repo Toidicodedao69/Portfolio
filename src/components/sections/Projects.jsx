0b4ea26e98e8409a7f4f0fb630e22d784e4bfdf1
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2025-04-01 19:59:37 +0700

    Update Projects details

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -71,7 +71,7 @@ export const Projects = () => {
                         </div>
 
                         <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                            <h3 className="text-xl font-bold mb-2"> Robot Navigation</h3>
+                            <h3 className="text-xl font-bold mb-2">Path Finding Engine</h3>
                             <p className="text-gray-400 mb-4">
                                 An object-oriented C# application to find optimal routes in wall mazes
                             </p>
